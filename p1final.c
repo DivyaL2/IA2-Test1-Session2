209d64e3319ae0502a03095d9744d9eb4bcdee78
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-10 15:04:03 +0000

    area of a triangle in final

---
+++ p1final.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+void input(float *b,float *h)
+{
+  printf("enter the value of base and height\n");
+  scanf("%f %f",&b,&h);
+}
+void find_area(float b,float h,float*area)
+
+{
+   *area=(b*h)/2;
+}
+void output(float base,float height,float area)
+{
+  printf("The area of a triangle is %f",area);
+}
+int main()
+{
+  float x,y,z;
+  input(&x,&y);
+  find_area(x,y,&z);
+  output(x,y,z);
+  return 0;
+
+}
